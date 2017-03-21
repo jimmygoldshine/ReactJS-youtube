7a675fd660d29f10aa71f5a8632cca9602cb6e34
James <jamesdix@cwgsy.net>
2017-03-21 18:30:49 +0000

    event handlers created

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = "AIzaSyAQoUx6fA4Hsfp_srveQZLo2yDEqpiz4wI"
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This companent should produce
+// some HTML.
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+// Take this componenet's HTML and put
+// it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
