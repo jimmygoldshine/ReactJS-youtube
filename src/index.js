6ef7b2f1b4035977ff0264b23b4bb8dc29145e6e
James <jamesdix@cwgsy.net>
2017-03-22 16:20:02 +0000

    videos added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = "AIzaSyAQoUx6fA4Hsfp_srveQZLo2yDEqpiz4wI";
 
-const API_KEY = "AIzaSyAQoUx6fA4Hsfp_srveQZLo2yDEqpiz4wI"
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// Create a new component. This companent should produce
-// some HTML.
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.videoSearch('animal friends')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-// Take this componenet's HTML and put
-// it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
